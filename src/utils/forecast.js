62e2637310c61b6fa23f4b658931dc9f1c1c24a3
leungmind11 <49142590+leungmind11@users.noreply.github.com>
2019-07-16 15:01:53 +0800

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,8 +12,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
+
             callback(undefined, body.daily.data[0].summary + "It is currently " + body.currently.temperature + 
-            " degrees out. There is a " + body.currently.precipProbability + " % chance of rain." + " The highest temperature is: " + body.daily.data[0].temperatureHigh + " degrees. The lowest temperature is: " + body.daily.data[0].temperatureLow + " degrees.")
+            " degrees out. The high today is " + body.daily.data[0].temperatureHigh + " with a low of " + body.daily.data[0].temperatureLow + ". There is a " + body.currently.precipProbability + " % chance of rain.")
         }
     })
 }
